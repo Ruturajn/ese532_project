7c1c4b05dc6ff039e82cac52e0ed2f2a387d9632
Ruturajn <nanotiruturaj@gmail.com>
2023-12-06 18:58:30 -0500

    update: 13-bit bit packing implemenation to remove lazy shifting

--- project/Temp_Files/bit_packing.c
+++ project/Temp_Files/bit_packing.c
@@ -8,6 +8,11 @@
 
 int main() {
 
+    // 0000 0000 1111 1111 // 0000 1011 1101 1111 // 0000 0111 1101 0000 // 0000 0011 1110 1000 // 0000 0000 0000 0001
+    // 0 0000 1111 1111 // 0 1011 1101 1111 // 0 0111 1101 0000 // 0 0011 1110 1000 // 0 0000 0000 0001
+    // 0000 0111 1111 1010 1111 0111 1100 1111 1010 0000 0011 1110 1000 0000 0000 0000 1
+    // 0    7    f     a    f    7    C   f    a     0    3    E    8    0    0
+    // 0 1
     uint16_t codes_arr[LZW_CODES_LEN] = { 255, 3039, 2000, 1000, 1};
     int data_len = ((LZW_CODES_LEN * 13) / 8) + 1;
 
@@ -35,7 +40,7 @@ int main() {
 
         if (bits_left == 0 && current_val_bits_left == 5) {
             if (data_idx < data_len) {
-                data[data_idx] = (current_val & 0x1F);
+                data[data_idx] = (current_val & 0x1F) << 3;
                 bits_left = 3;
                 current_val_bits_left = 0;
                 continue;
@@ -44,7 +49,7 @@ int main() {
         }
 
         if (bits_left == 3 && current_val_bits_left == CODE_LENGTH) {
-            data[data_idx] = ((data[data_idx] << 3) | (((current_val >> 10)) & 0x07));
+            data[data_idx] |= ((current_val >> 10) & 0x07);
             bits_left = 0;
             data_idx += 1;
             current_val_bits_left = 10;
@@ -62,7 +67,7 @@ int main() {
 
         if (bits_left == 0 && current_val_bits_left == 2) {
             if (data_idx < data_len) {
-                data[data_idx] = (current_val & 0x03);
+                data[data_idx] = (current_val & 0x03) << 6;
                 bits_left = 6;
                 current_val_bits_left = 0;
                 continue;
@@ -71,7 +76,7 @@ int main() {
         }
 
         if (bits_left == 6 && current_val_bits_left == CODE_LENGTH) {
-            data[data_idx] = ((data[data_idx] << 6) | (((current_val >> 7)) & 0x3F));
+            data[data_idx] |= ((current_val >> 7) & 0x3F);
             bits_left = 0;
             data_idx += 1;
             current_val_bits_left = 7;
@@ -79,7 +84,7 @@ int main() {
 
         if (bits_left == 0 && current_val_bits_left == 7) {
             if (data_idx < data_len) {
-                data[data_idx] = (current_val & 0x7F);
+                data[data_idx] = (current_val & 0x7F) << 1;
                 bits_left = 1;
                 current_val_bits_left = 0;
                 continue;
@@ -88,7 +93,7 @@ int main() {
         }
 
         if (bits_left == 1 && current_val_bits_left == CODE_LENGTH) {
-            data[data_idx] = ((data[data_idx] << 1) | ((current_val >> 12) & 0x1));
+            data[data_idx] |= ((current_val >> 12) & 0x1);
             bits_left = 0;
             data_idx += 1;
             current_val_bits_left = 12;
@@ -106,7 +111,7 @@ int main() {
 
         if (bits_left == 0 && current_val_bits_left == 4) {
             if (data_idx < data_len) {
-                data[data_idx] = (current_val & 0x0F);
+                data[data_idx] = (current_val & 0x0F) << 4;
                 bits_left = 4;
                 current_val_bits_left = 0;
                 continue;
@@ -115,7 +120,7 @@ int main() {
         }
 
         if (bits_left == 4 && current_val_bits_left == CODE_LENGTH) {
-            data[data_idx] = ((data[data_idx] << 4) | ((current_val >> 9) & 0x0F));
+            data[data_idx] |= ((current_val >> 9) & 0x0F);
             data_idx += 1;
             bits_left = 0;
             current_val_bits_left = 9;
@@ -132,14 +137,14 @@ int main() {
         }
 
         if (bits_left == 0 && current_val_bits_left == 1) {
-            data[data_idx] = ((data[data_idx] << 1) | (current_val & 0x01));
+            data[data_idx] = (current_val & 0x01) << 7;
             bits_left = 7;
             current_val_bits_left = 0;
             continue;
         }
 
         if (bits_left == 7 && current_val_bits_left == CODE_LENGTH) {
-            data[data_idx] = ((data[data_idx] << 7) | ((current_val >> 6) & 0x7F));
+            data[data_idx] |= ((current_val >> 6) & 0x7F);
             bits_left = 0;
             current_val_bits_left = 6;
             data_idx += 1;
@@ -147,7 +152,7 @@ int main() {
 
         if (bits_left == 0 && current_val_bits_left == 6) {
             if (data_idx < data_len) {
-                data[data_idx] = ((current_val >> 7) & 0x3F);
+                data[data_idx] = ((current_val >> 7) & 0x3F) << 2;
                 bits_left = 2;
                 current_val_bits_left = 0;
                 continue;
@@ -157,7 +162,7 @@ int main() {
 
         if (bits_left == 2 && current_val_bits_left == CODE_LENGTH) {
             if (data_idx < data_len) {
-                data[data_idx] = ((current_val >> 11) & 0x03);
+                data[data_idx] |= ((current_val >> 11) & 0x03);
                 bits_left = 0;
                 data_idx += 1;
                 current_val_bits_left = 11;
